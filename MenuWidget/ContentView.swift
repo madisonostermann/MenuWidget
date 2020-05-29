347ff527a004941977351ad8604127f53a94e2f2
mgipson <mmgipson3@gmail.com>
2020-05-29 09:58:57 -0400

    Initial Commit

---
+++ MenuWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MenuWidget
+//
+//  Created by Madison Gipson on 5/29/20.
+//  Copyright © 2020 Madison Gipson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
